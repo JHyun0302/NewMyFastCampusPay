1505be6b55ad80e80f96d8f0e14258333be42b4e
JHyun0302 <wogustkfkd11@gmail.com>
2024-12-12 22:43:59 +0900

    :sparkles: [Feat] Kafka

---
+++ banking-service/src/main/java/com/newfastcampuspay/banking/BankingConfig.java
@@ -0,0 +1,9 @@
+package com.newfastcampuspay.banking;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.newfastcampuspay.common")
+public class BankingConfig {
+}
---
+++ membership-service/src/main/java/com/newfastcampuspay/membership/MembershipConfig.java
@@ -0,0 +1,9 @@
+package com.newfastcampuspay.membership;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.newfastcampuspay.common")
+public class MembershipConfig {
+}
